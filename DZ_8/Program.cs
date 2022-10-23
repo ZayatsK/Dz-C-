79c2c2592e5d60615537ba59e9dc7151e6ee6997
Konstantin <zorex2008@yandex.ru>
2022-10-23 13:38:01 +0700

    Задача №8 готова

---
+++ DZ_8/Program.cs
@@ -0,0 +1,23 @@
+﻿/*адача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.WriteLine("Введите число");
+string numAstr = Console.ReadLine();
+int numA = int.Parse(numAstr);
+
+if (numA <= 0)
+    {
+        Console.WriteLine("Введите число больше 0");
+    }
+
+for (int i = 1; i <= numA; i++)
+{
+    if (i%2==0)
+    {
+        Console.WriteLine(i);
+    }
+    
+    
+}
