4395c5f8862255f62ab9180e5edc390f29fcced7
Konstantin <zorex2008@yandex.ru>
2022-10-31 23:38:30 +0700

    Задача №21 готова

---
+++ DZ_21/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+double GetDistance (int x1, int y1, int c1, int x2, int y2, int c2) // возращаем дабл потому что не целое число
+{
+double distance = Math.Sqrt(Math.Pow(y2 - y1, 2) + Math.Pow(x2 - x1, 2) + Math.Pow(c2 - c1, 2)); //формула растояния между тчк 3д пространства
+return distance;
+}
+
+int GetNumber (string message) 
+{
+int userNumber;
+while (true) 
+{
+    Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out int number)) //проверка на число
+        {
+            userNumber = number; // вкладываем в переменную если введенно число
+            break;
+        }
+        else 
+        {
+            Console.WriteLine($"{number} не является числом, повторите ввод.");
+        }
+}
+return userNumber; //возращаем число
+}
+
+int x1Number = GetNumber("Введите координату X1 - ");
+int y1Number = GetNumber("Введите координату Y1 - ");
+int c1Number = GetNumber("Введите координату C1 - ");
+int x2Number = GetNumber("Введите координату X2 - ");
+int y2Number = GetNumber("Введите координату Y2 - ");
+int c2Number = GetNumber("Введите координату C2 - ");
+
+double myDistance = GetDistance(x1Number, y1Number,c1Number, x2Number, y2Number, c2Number);
+
+Console.WriteLine($"Расстояние между точками ({x1Number}, {y1Number}, {c1Number}) и ({x2Number}, {y2Number}, {c2Number}) составляет {Math.Round(myDistance, 2)}.");
+//Math.Round определяем сколько нужно знаков после запятой
